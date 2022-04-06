357e37f7532c78d03e0dcde6f42c53b404bdc8c7
Oleg Semyonov <oleg@prezly.com>
2022-04-06 10:16:52 +0300

    Add support for Placeholder nodes

--- components/SlateRenderer/SlateRenderer.tsx
+++ components/SlateRenderer/SlateRenderer.tsx
@@ -1,15 +1,23 @@
+import type { ComponentRenderers } from '@prezly/content-renderer-react-js';
 import { Renderer } from '@prezly/content-renderer-react-js';
 import type { Node } from '@prezly/slate-types';
+import { PLACEHOLDER_NODE_TYPE } from '@prezly/slate-types';
 import '@prezly/content-renderer-react-js/styles.css';
+
+import { Placeholder } from './components';
 
 interface Props {
     nodes: Node | Node[];
 }
 
+const components: ComponentRenderers = {
+    [PLACEHOLDER_NODE_TYPE]: Placeholder,
+};
+
 export function SlateRenderer({ nodes }: Props) {
     return (
         <div>
-            <Renderer nodes={nodes} />
+            <Renderer nodes={nodes} components={components} />
         </div>
     );
 }
--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
 export { PageSeo } from './PageSeo';
 export { Pagination } from './Pagination';
 export { SlateRenderer } from './SlateRenderer';
+export { StoryPublicationDate } from './StoryPublicationDate';
 export { StorySeo } from './StorySeo';
