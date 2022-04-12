1038694d624f898fe9cb93ed0c26313e981b08b8
Oleg Semyonov <oleg@prezly.com>
2022-04-12 18:28:43 +0300

    Use infinite loading logic from theme-kit

--- pages/api/fetch-galleries.ts
+++ pages/api/fetch-galleries.ts
@@ -1,25 +1,3 @@
-import { getPrezlyApi } from '@prezly/theme-kit-nextjs';
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-async function fetchGalleries(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== 'POST') {
-        res.status(405);
-        return;
-    }
-
-    const { page, pageSize } = req.body;
-
-    try {
-        const api = getPrezlyApi(req);
-
-        const { galleries } = await api.getGalleries({ page, pageSize });
-
-        res.status(200).json({ galleries });
-    } catch (error) {
-        res.status(500).send({
-            message: (error as Error).message,
-        });
-    }
-}
+import { fetchGalleries } from '@prezly/theme-kit-nextjs';
 
 export default fetchGalleries;
