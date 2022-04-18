9f08bb07dd9300d9978438e34c1ff536b10d3145
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-04-18 06:34:42 +0200

    Add support for Placeholder nodes and create Story Publication Date component

---
+++ components/StoryPublicationDate/index.ts
@@ -0,0 +1,1 @@
+export { default as StoryPublicationDate } from './StoryPublicationDate';
--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export { PageSeo } from './PageSeo';
 export { Pagination } from './Pagination';
 export { SlateRenderer } from './SlateRenderer';
 export { StorySeo } from './StorySeo';
+export { StoryPublicationDate } from './StoryPublicationDate';
