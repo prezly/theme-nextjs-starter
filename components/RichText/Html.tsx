a7d6d0330d5bdaaa59b7585ebb5c2a97dd72649b
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-04-18 09:48:29 +0200

    Delete unnecessary line

--- components/RichText/Html.tsx
+++ components/RichText/Html.tsx
@@ -1,12 +1,10 @@
 import { Html as HtmlRenderer } from '@prezly/content-renderer-react-js';
 import { type HtmlNode } from '@prezly/slate-types';
 
-import styles from './styles.module.scss';
-
 interface Props {
-  node: HtmlNode;
+    node: HtmlNode;
 }
 
 export function Html({ node }: Props) {
-  return <HtmlRenderer node={node} className={styles.htmlContent} />;
+    return <HtmlRenderer node={node} className={styles.htmlContent} />;
 }
