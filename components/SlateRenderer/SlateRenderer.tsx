f6949589c55a067d06ebe59010dbbe2e6365de9d
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-04-19 15:32:21 +0300

    Update code to the new content renderer
    See https://github.com/prezly/content-renderer-react-js/pull/19

--- components/SlateRenderer/SlateRenderer.tsx
+++ components/SlateRenderer/SlateRenderer.tsx
@@ -1,7 +1,6 @@
-import type { ComponentRenderers } from '@prezly/content-renderer-react-js';
-import { Renderer } from '@prezly/content-renderer-react-js';
+import { Component, Renderer } from '@prezly/content-renderer-react-js';
 import type { Node } from '@prezly/slate-types';
-import { PLACEHOLDER_NODE_TYPE } from '@prezly/slate-types';
+import { isPlaceholderNode } from '@prezly/slate-types';
 import '@prezly/content-renderer-react-js/styles.css';
 
 import { Placeholder } from './components';
@@ -10,14 +9,12 @@ interface Props {
     nodes: Node | Node[];
 }
 
-const components: ComponentRenderers = {
-    [PLACEHOLDER_NODE_TYPE]: Placeholder,
-};
-
 export function SlateRenderer({ nodes }: Props) {
     return (
         <div>
-            <Renderer nodes={nodes} components={components} />
+            <Renderer nodes={nodes}>
+                <Component match={isPlaceholderNode} component={Placeholder} />
+            </Renderer>
         </div>
     );
 }
